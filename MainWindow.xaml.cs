30e15bb478144c0802bcaac6c86229399e97f911
gobdanov <gobdanov@inbox.ru>
2026-04-20 15:57:12 +0500

    проиницализирован гит. богданов.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using KAMA_PRO_CRUD_APP.pages;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KAMA_PRO_CRUD_APP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window init1 = this;
+            mainWindow.Navigate(new Authorizing());
+        }
+    }
+}
---
+++ MainWindow1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KAMA_PRO_CRUD_APP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow1.xaml
+    /// </summary>
+    public partial class MainWindow1 : Window
+    {
+        public MainWindow1()
+        {
+            InitializeComponent();
+            MainWindow1 init2 = this;
+        }
+    }
+}
